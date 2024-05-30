668f2ac72febb7c5ff54e91cf61c3ff2e01adb20
Drew Rautenberg <drew@drewrautenberg.com>
2024-05-30 10:34:31 -0500

    renamed vars to all snake case

--- src/api.py
+++ src/api.py
@@ -12,18 +12,18 @@ def read_root():
 
 @app.get("/lines")
 def read_root():
-    with open('../data/lines.csv', newline='') as lineCSV:
-        csv_read = csv.DictReader(lineCSV)
-        lineList =[row for row in csv_read]
-    return lineList
+    with open('../data/lines.csv', newline='') as line_csv:
+        csv_read = csv.DictReader(line_csv)
+        line_list =[row for row in csv_read]
+    return line_list
 
 
 @app.get("/Stations/{line}")
 def read_item(line: str,):
-    with open(f"../data/{line}.csv" , newline='') as lineCSV:
-        csv_read = csv.DictReader(lineCSV)
-        lineList =[row for row in csv_read]
-    return lineList
+    with open(f"../data/{line}.csv" , newline='') as station_csv:
+        csv_read = csv.DictReader(station_csv)
+        station_List =[row for row in csv_read]
+    return station_List
 
 @app.get("/Trains/{map_id}")
 async def read_item(map_id: str,):
