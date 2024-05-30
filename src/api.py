462ca09e0fb0c9c5255749ac0d66e9b68cfecf8d
Drew Rautenberg <drew@drewrautenberg.com>
2024-05-29 21:26:56 -0500

    code, but errors

--- src/api.py
+++ src/api.py
@@ -26,10 +26,8 @@ def read_item(line: str,):
     return lineList
 
 @app.get("/Trains/{map_id}")
-def read_item(map_id: str,):
+async def read_item(map_id: str,):
     api_key = key.key
     max_results = 6
-    api_url = f"http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" \
-              f"{api_key}&mapid={map_id}&max={max_results}&outputType=JSON"
-    response = requests.get(api_url, timeout=30)
-    return response
+    api_url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={api_key}&mapid={map_id}&max={max_results}&outputType=JSON"
+    return requests.get(api_url, timeout=30)
